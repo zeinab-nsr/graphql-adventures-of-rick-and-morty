db0a495da5f25f2b3d47e3283c6b416b43ada2f2
Zeinab Nasiri <zeinabb@Zeinabs-MacBook-Pro.local>
2023-03-28 11:10:17 +0330

    feat: add debounce helper functio + unit tests

---
+++ src/helpers/debounce.js
@@ -0,0 +1,14 @@
+export default function debounce(func, delay) {
+  let timerId;
+
+  return function(...args) {
+    if (timerId) {
+      clearTimeout(timerId);
+    }
+
+    timerId = setTimeout(() => {
+      func.apply(this, args);
+      timerId = null;
+    }, delay);
+  }
+}
