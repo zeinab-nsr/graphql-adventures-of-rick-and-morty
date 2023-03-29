4792ff1d299f8447164238fc6b81c9d6a544a437
Zeinab Nasiri <zeinabb@Zeinabs-MacBook-Pro.local>
2023-03-29 13:51:23 +0330

    fix: fix tailwind setup

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 import { BrowserRouter } from 'react-router-dom';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import './index.css';
 
 const client = new ApolloClient({
   uri: "https://rickandmortyapi.com/graphql",
